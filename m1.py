e1f1e22412aef0e78dc92f9c1c3e9592900fb356
pastafarista <antonio.cabrera.landin@gmail.com>
2023-12-20 14:04:12 +0100

    feature: Creado el map de la primera consulta

--- m1.py
+++ m1.py
@@ -1,6 +1,18 @@
 #!/home/antonio/.anaconda3/bin/python
 
 import sys
+ 
+lineas_filtradas = []
+ 
+for line in sys.stdin:    
+    columnas = line.split(";")
+       
+    if(len(columnas) > 4 and columnas[4] == "GET"):
+            lineas_filtradas.append(line)
 
-for line in sys.stdin:
-	print(line)
+# Devolvemos las líneas filtradas a la salida estándar
+sys.stdout.writelines(lineas_filtradas)
+    
+    
+ 
+    
