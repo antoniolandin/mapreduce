06f8c3b4844fb0ad08cdf10246b782dc027728d0
Alejandro Gómez García <cj124357689@gmail.com>
2023-12-20 14:25:36 +0100

    r1.py

--- m1.py
+++ m1.py
@@ -1,18 +1,6 @@
 #!/home/antonio/.anaconda3/bin/python
 
 import sys
- 
-lineas_filtradas = []
- 
-for line in sys.stdin:    
-    columnas = line.split(";")
-       
-    if(len(columnas) > 4 and columnas[4] == "GET"):
-            lineas_filtradas.append(line)
 
-# Devolvemos las líneas filtradas a la salida estándar
-sys.stdout.writelines(lineas_filtradas)
-    
-    
- 
-    
+for line in sys.stdin:
+	print(line)
--- r1.py
+++ r1.py
@@ -2,5 +2,9 @@
 
 import sys
 
+count = 0
+
 for line in sys.stdin:
-	print(line)
+	count  += 1
+
+print(count)
